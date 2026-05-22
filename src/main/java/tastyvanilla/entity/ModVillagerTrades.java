16fd07669823ad7fd48879d6aeded3d84b8b5059
Jaubert <ismael.rfares@gmail.com>
2026-05-22 06:38:41 -0700

    Minor Commit

--- src/main/java/tastyvanilla/entity/ModVillagerTrades.java
+++ src/main/java/tastyvanilla/entity/ModVillagerTrades.java
@@ -19,52 +19,52 @@ public class ModVillagerTrades {
             factories.add((entity,random) -> new TradeOffer(
                     new TradedItem(ModItems.BERRY_BLACKBERRIES, 10),
                     new ItemStack(Items.EMERALD, 1),12, 30, 0.05F));
-             factories.add((entity,random) -> new TradeOffer(
+            factories.add((entity,random) -> new TradeOffer(
                     new TradedItem(ModItems.BERRY_BLUEBERRIES, 10),
                     new ItemStack(Items.EMERALD, 1),12, 30, 0.05F));
-             factories.add((entity,random) -> new TradeOffer(
+            factories.add((entity,random) -> new TradeOffer(
                     new TradedItem(ModItems.BERRY_ELDERBERRIES, 10),
                     new ItemStack(Items.EMERALD, 1),12, 30, 0.05F));
-             factories.add((entity,random) -> new TradeOffer(
+            factories.add((entity,random) -> new TradeOffer(
                     new TradedItem(ModItems.BERRY_GOJI_BERRIES, 10),
                     new ItemStack(Items.EMERALD, 1),12, 30, 0.05F));
-             factories.add((entity,random) -> new TradeOffer(
+            factories.add((entity,random) -> new TradeOffer(
                     new TradedItem(ModItems.BERRY_GOOSEBERRIES, 10),
                     new ItemStack(Items.EMERALD, 1),12, 30, 0.05F));
-             factories.add((entity,random) -> new TradeOffer(
+            factories.add((entity,random) -> new TradeOffer(
                     new TradedItem(ModItems.BERRY_STRAWBERRIES, 10),
                     new ItemStack(Items.EMERALD, 1),12, 30, 0.05F));
-             factories.add((entity,random) -> new TradeOffer(
+            factories.add((entity,random) -> new TradeOffer(
                     new TradedItem(ModItems.BERRY_RASPBERRIES, 10),
                     new ItemStack(Items.EMERALD, 1),12, 30, 0.05F));
-             factories.add((entity,random) -> new TradeOffer(
+            factories.add((entity,random) -> new TradeOffer(
                     new TradedItem(ModItems.BERRY_WHITE_CURRANT_BERRIES, 10),
                     new ItemStack(Items.EMERALD, 1),12, 30, 0.05F));
         });
 
         TradeOfferHelper.registerVillagerOffers(VillagerProfession.FARMER, 1, factories -> {
-             factories.add((entity,random) -> new TradeOffer(
+            factories.add((entity,random) -> new TradeOffer(
                     new TradedItem(ModItems.CABBAGE, 20),
                     new ItemStack(Items.EMERALD, 1),16, 2, 0.5F));
-             factories.add((entity,random) -> new TradeOffer(
+            factories.add((entity,random) -> new TradeOffer(
                     new TradedItem(ModItems.CHILLI, 22),
                     new ItemStack(Items.EMERALD, 1),16, 2, 0.5F));
-             factories.add((entity,random) -> new TradeOffer(
+            factories.add((entity,random) -> new TradeOffer(
                     new TradedItem(ModItems.EGGPLANT, 20),
                     new ItemStack(Items.EMERALD, 1),16, 2, 0.5F));
-             factories.add((entity,random) -> new TradeOffer(
+            factories.add((entity,random) -> new TradeOffer(
                     new TradedItem(ModItems.GARLIC, 20),
                     new ItemStack(Items.EMERALD, 1),16, 2, 0.5F));
-             factories.add((entity,random) -> new TradeOffer(
+            factories.add((entity,random) -> new TradeOffer(
                     new TradedItem(ModItems.LETTUCE, 26),
                     new ItemStack(Items.EMERALD, 1),16, 2, 0.5F));
-             factories.add((entity,random) -> new TradeOffer(
+            factories.add((entity,random) -> new TradeOffer(
                     new TradedItem(ModItems.ONION, 20),
                     new ItemStack(Items.EMERALD, 1),16, 2, 0.5F));
-             factories.add((entity,random) -> new TradeOffer(
+            factories.add((entity,random) -> new TradeOffer(
                     new TradedItem(ModItems.TOMATO, 22),
                     new ItemStack(Items.EMERALD, 1),16, 2, 0.5F));
-             factories.add((entity,random) -> new TradeOffer(
+            factories.add((entity,random) -> new TradeOffer(
                     new TradedItem(ModItems.SWEET_POTATO, 26),
                     new ItemStack(Items.EMERALD, 1),16, 2, 0.5F));
         });
